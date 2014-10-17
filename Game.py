5c75dcb87000a505d5bf685b57d7b2f2ac6207f0
Emil Kattainen <emilkattainen@gmail.com>
2014-10-17 19:48:34 +0300

    Shaishe

--- Game.py
+++ Game.py
@@ -1,21 +1,40 @@
 import cv2
 import numpy as np
 import math
+import random as rng
+
 class Game:
+	RAD = 20
+	SHOTRAD = 7
 	def __init__(self):
-		self.x = 0
-		self.y = 0
-		self.rot = 0
-	def forward(self):
-		self.x += 10*math.cos(self.rot)
-		self.y += 10*math.sin(self.rot)
-		print self.x
-		print self.y
-	def turn(self,amt):
-		self.rot += amt
+		self.fighters = []
+		self.fighters.append([100,100,0])
+		self.fighters.append([500,500,0])
+		self.shots = []
+	def forward(self,fighter):
+		self.fighters[fighter][0] += 10*math.cos(self.fighters[fighter][2])
+		self.fighters[fighter][1] += 10*math.sin(self.fighters[fighter][2])
+	def turn(self,fighter,amt):
+		self.fighters[fighter][2] += amt
+	def shoot(self,fighter):
+		self.shots.append([self.fighters[fighter][0],self.fighters[fighter][1],self.fighters[fighter][2],fighter])
+	def checkCollisions(self):
+		keep = []
+		for fighter in self.fighters:
+			for shot in self.shots:
+				if self.fighters[shot[3]] != fighter:
+					if not (math.sqrt(math.pow(fighter[0]-shot[0],2)+math.pow(fighter[1]-shot[1],2)) < self.RAD):
+						keep.append(shot)
+		self.shots = keep;
 	def update(self):
-		print 'sus'
+		self.checkCollisions()
+		for shot in self.shots:
+			shot[0] += 10*math.cos(shot[2])
+			shot[1] += 10*math.sin(shot[2])
 	def render(self):
 		view = np.zeros((720,1280,3),np.uint8)
-		cv2.circle(view,(int(self.x),int(self.y)),100,(250,175,190))
+		cv2.circle(view,(int(self.fighters[0][0]),int(self.fighters[0][1])),self.RAD,(250,175,190),-1)
+		cv2.circle(view,(int(self.fighters[1][0]),int(self.fighters[1][1])),self.RAD,(0,0,255),-1)
+		for shot in self.shots:
+			cv2.circle(view,(int(shot[0]),int(shot[1])),self.SHOTRAD,(0,128,255),-1)
 		cv2.imshow('saas',view)
--- Main.py
+++ Main.py
@@ -4,16 +4,19 @@ class Main:
 	UP = 119
 	LEFT = 97
 	RIGHT = 100
+	SPACE = 32
 	game = Game()
 	def getInput(self):
 		key = cv2.waitKey(1)
 		print key
 		if key == self.UP:
-			self.game.forward()
+			self.game.forward(0)
 		if key == self.LEFT:
-			self.game.turn(-0.1)
+			self.game.turn(0,-0.1)
 		if key == self.RIGHT:
-			self.game.turn(0.1)
+			self.game.turn(0,0.1)
+		if key == self.SPACE:
+			self.game.shoot(0)
 	def update(self):
 		self.game.update()
 	def render(self):
@@ -25,7 +28,6 @@ class Main:
 			self.update()
 			self.render()
 		
-	
 if __name__ == '__main__':
 	main = Main()
 	main.run()
