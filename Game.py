f8b4821e7233c2e602369ca80dfee71db01cbbbc
Emil Kattainen <emilkattainen@gmail.com>
2014-10-29 15:11:36 +0200

    paskaa

--- Game.py
+++ Game.py
@@ -6,6 +6,7 @@ import random as rng
 class Game:
 	RAD = 20
 	SHOTRAD = 7
+	VDIST = 50
 	def __init__(self):
 		self.fighters = []
 		self.fighters.append([100,100,0])
@@ -25,6 +26,8 @@ class Game:
 				if self.fighters[shot[3]] != fighter:
 					if not (math.sqrt(math.pow(fighter[0]-shot[0],2)+math.pow(fighter[1]-shot[1],2)) < self.RAD):
 						keep.append(shot)
+					else:
+						fighters[shot[3]]
 		self.shots = keep;
 	def update(self):
 		self.checkCollisions()
@@ -38,3 +41,33 @@ class Game:
 		for shot in self.shots:
 			cv2.circle(view,(int(shot[0]),int(shot[1])),self.SHOTRAD,(0,128,255),-1)
 		cv2.imshow('saas',view)
+	def getView(self,fighter):
+		sees = np.zeros((3),np.uint8)
+		x0 = self.fighters[fighter]
+		y0 = self.fighters[fighter]
+		x1 = self.fighters[1][0]
+		y1 = self.fighters[1][1]
+		k1 = math.tan(self.fighters[fighter][2])
+		k2 = k1 - math.PI/8
+		k3 = k1 + math.PI/8
+		k4 = y1-y0 / x1 - x0 if x1-x0 != 0 else abs(y1-y0)/y1-y0 * math.PI / 2
+		a0 = math.atan(abs((k1-k4)/1 + k1*k4))
+		a1 = math.atan(abs((k2-k4)/1 + k2*k4))
+		a2 = math.atan(abs((k3-k4)/1 + k3*k4))
+		d = math.sqrt((y1-y0) ** 2 + (x1-x0) ** 2)
+		'''
+		c1 = y0 - k1 * x0
+		c2 = y0 - k2 * x0
+		c3 = y0 - k3 * x0
+		d1 = math.abs(k1 * x1 + y1 + c1)/math.sqrt(k ** 2 + 1)
+		d2 = math.abs(k2 * x1 + y1 + c2)/math.sqrt(k ** 2 + 1)
+		d3 = math.abs(k3 * x1 + y1 + c3)/math.sqrt(k ** 2 + 1)
+		'''
+		if d * math.sin(a0) < RAD:
+			sees[1] = min(math.sqrt((x0 - x1) ** 2 + (y0 - y1) ** 2),VDIST)
+		if d * math.sin(a1) < RAD:
+			sees[0] = min(math.sqrt((x0 - x1) ** 2 + (y0 - y1) ** 2),VDIST)
+		if d * math.sin(a2) < RAD:
+			sees[2] = min(math.sqrt((x0 - x1) ** 2 + (y0 - y1) ** 2),VDIST)
+	def getReward(fighter):
+		return 1
